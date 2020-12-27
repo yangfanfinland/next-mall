354c0c5df1b076798cd9dfdc2d331d074544c9e0
yangfanfinland <yangfanfinland@gmail.com>
2020-12-27 19:08:39 +0200

    Back to main page

--- components/SearchArea/index.tsx
+++ components/SearchArea/index.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import SearchBar from '../SearchBar'
+import Link from 'next/link'
 import styles from './index.less'
 
-const SearchArea = ({ keywords, noSearch = false }: { keywords?: string, noSearch?: boolean }) => {
-    return (
-        <div className={`bw`}>
-            <div className={`${styles.searchWrap} contentWidth`}>
-                <a className={styles.logoWrap}>
-                    <img src="/static/images/logobig.png" alt="米桶电商"/>
-                </a>
-                {
-                    !noSearch &&
-                    <div className={styles.searchArea}>
-                        <SearchBar defaultValue={keywords} />
-                    </div>
-                }
-            </div>
-        </div>
-    )
+const SearchArea = ({
+  keywords,
+  noSearch = false,
+}: {
+  keywords?: string
+  noSearch?: boolean
+}) => {
+  return (
+    <div className={`bw`}>
+      <div className={`${styles.searchWrap} contentWidth`}>
+        <Link href={`/`}>
+          <a className={styles.logoWrap}>
+            <img src="/static/images/logobig.png" alt="米桶电商" />
+          </a>
+        </Link>
+
+        {!noSearch && (
+          <div className={styles.searchArea}>
+            <SearchBar defaultValue={keywords} />
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default SearchArea
